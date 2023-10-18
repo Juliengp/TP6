9fac0a9fe90b64362b70f5a835a4f9eb85243e99
Juliengp <145107152+Juliengp@users.noreply.github.com>
2023-10-18 18:00:17 +0200

    Update TicketMachineTest.java

--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -30,4 +30,79 @@ class TicketMachineTest {
 		assertEquals(10 + 20, machine.getBalance(), "La balance n'est pas correctement mise à jour");              
 	}
 
+	@Test
+
+	void nImprimerPasSimontantInsuffisant(){
+		machine.insertMoney( PRICE -1);
+		assertFalse(machine.printTicket(),"Le ticket ne doit pas être imprimé");
+	}
+
+	@Test
+
+	void imprimeSiMontantSuffisant(){
+		machine.insertMoney(PRICE +1);
+		assertTrue(machine.printTicket(),"Le ticket aurait dû être imprimé");
+	}
+
+	@Test
+
+	void decrementationQuandTicketImprime(){
+		machine.insertMoney(PRICE + 10);
+		machine.printTicket();
+		assertEquals(10,machine.getBalance(), "La balance aurait dû être décrémentée");
+	}
+
+	@Test
+
+	void montantMisAJourQuandTicketImprime(){
+		machine.insertMoney(PRICE);
+		machine.printTicket();
+		assertEquals(PRICE,machine.getTotal(),"le montant n'a pas été mis à jour");
+	}
+
+	@Test
+
+	void montantPasMisAJourQuandTicketNonImprime(){
+		machine.insertMoney(PRICE);
+		assertEquals(0,machine.getTotal(),"le montant n'aurais pas dû être mis à jour");
 }
+
+	@Test
+
+	void rendCorrectementLaMonnaie(){
+		machine.insertMoney(PRICE+10);
+		machine.printTicket();
+		assertEquals(10,machine.refund(),"La monnaie n'a pas été rendue");
+	}
+
+	@Test
+
+	void remetLaBalanceAZero(){
+		machine.insertMoney(PRICE+10);
+		machine.printTicket();
+		machine.refund();
+		assertEquals(0,machine.getBalance(),"La balance n'a pas été remise à zéro");
+	}
+
+	@Test
+
+	void onNePeutPasInsererDargentNegatif(){
+		try{
+			machine = new TicketMachine(-50);
+			fail("Argument mus not be negative");}
+			catch(IllegalArgumentException e){};
+		}
+	
+
+	@Test
+
+	void pasTicketNeg(){
+		try{
+			machine = new TicketMachine(-30);
+			fail("Argument mus not be negative");}
+			catch(IllegalArgumentException e){};
+		}
+	
+	}
+
+
